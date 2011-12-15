4f120501eb7361562a25805c12ed2a909246019f
Iaian Weber <iaianrw@gmail.com>
2011-12-15 16:49:34 -0700

    v2 less lines

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,29 +1,21 @@
 #!/usr/bin/env ruby
+# V2. Less lines
 
 print "\n FizzBuzz cheatsheet :>o<: \n\n"
 
 for i in (1..100)
-  # for formatting fix, probably a better way to do this..
-  buzzed = 0
-  
-  # this i % 3 == 0 part took me a bit to discover.. 
-  # basically it devides and checks for a remander, a good thing to learn about. 
-  
-  if i % 15 == 0
-    print "#{i} FizzBuzz\n"
-    buzzed = 1
-  end
+  # To set the buzzer as i if all if's fail ( is not multiple of 3 or 5 )
+  buzzer = i
   if i % 3 == 0
-    print "#{i} Fizz\n"
-    buzzed = 1
+    # #{var} is how to integrate variables within strings
+    buzzer = "#{i} fizz"
   end
   if i % 5 == 0
-    print "#{i} Buzz\n"
-    buzzed = 1
+    buzzer = "#{i} buzz"
   end
-  
-  # checks if a label has been added, if not put i. 
-  if buzzed == 0
-    puts i
+  if i % 15 == 0
+    buzzer = "#{i} fizzbuzz"
   end
+  # displays buzzer and all contents
+  puts "#{buzzer}"
 end
