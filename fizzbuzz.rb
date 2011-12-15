f78ade0e3e883bb169390c6f83d5f6d92b00841b
Iaian Weber <iaianrw@gmail.com>
2011-12-15 16:28:24 -0700

    fixed some unneeded code and added comments

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,20 +3,26 @@
 print "\n FizzBuzz cheatsheet :>o<: \n\n"
 
 for i in (1..100)
-  # for formatting fix
+  # for formatting fix, probably a better way to do this..
   buzzed = 0
-  if i % 15 == 0 then
+  
+  # this i % 3 == 0 part took me a bit to discover.. 
+  # basically it devides and checks for a remander, a good thing to learn about. 
+  
+  if i % 15 == 0
     print "#{i} FizzBuzz\n"
     buzzed = 1
   end
-  if i % 3 == 0 then
+  if i % 3 == 0
     print "#{i} Fizz\n"
     buzzed = 1
   end
-  if i % 5 == 0 then
+  if i % 5 == 0
     print "#{i} Buzz\n"
     buzzed = 1
   end
+  
+  # checks if a label has been added, if not put i. 
   if buzzed == 0
     puts i
   end
